55a2b818ffb08ab80f50dfa94183d48b3c47b035
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-05-11 23:46:44 +0900

    add Constructor

--- src/JirungEngine/BoxCollider/BoxCollider.cpp
+++ src/JirungEngine/BoxCollider/BoxCollider.cpp
@@ -3,3 +3,10 @@
 using namespace JirungEngine;
 
 
+BoxCollider::BoxCollider() : top_left { 0, 0 }, buttom_right { 0, 0 } {
+
+}
+
+BoxCollider::BoxCollider(const Point& top_left, const Point& buttom_right) : top_left { top_left }, buttom_right { buttom_right } {
+
+}
--- src/JirungEngine/BoxCollider/BoxCollider.hpp
+++ src/JirungEngine/BoxCollider/BoxCollider.hpp
@@ -1,9 +1,17 @@
 #ifndef __BoxCollider_hpp__
 #define __BoxCollider_hpp__
 
+#include "../Point/Point.hpp"
+
+
 namespace JirungEngine {
     class BoxCollider {
+        public: Point top_left;
+        public: Point buttom_right;
 
+    public:
+        BoxCollider();
+        BoxCollider(const Point& top_left, const Point& buttom_right);
     };
 }
 
