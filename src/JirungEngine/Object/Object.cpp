e6574eeedc528b3a68535e882bdaf3351cb8b92d
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-05-12 00:19:54 +0900

    Object

--- src/JirungEngine/Object/Object.cpp
+++ src/JirungEngine/Object/Object.cpp
@@ -1,5 +1,22 @@
 #include "Object.hpp"
 
+using namespace std;
 using namespace JirungEngine;
 
 
+Object::Object(string id, const Object* parent_object) : id { id }, position { 0, 0 }, box_collider { nullptr }, parent_object { parent_object } {
+    object_id_list.push_back(id);
+}
+
+Object::~Object() {
+    object_id_list.remove(id);
+}
+
+
+Object* Object::getParent() {
+    return parent_object;
+}
+
+Object* Object::getChildById(string id) {
+    
+}
--- src/JirungEngine/Object/Object.hpp
+++ src/JirungEngine/Object/Object.hpp
@@ -1,9 +1,31 @@
 #ifndef __Object_hpp__
 #define __Object_hpp__
 
+#include "../Point/Point.hpp"
+#include "../BoxCollider/BoxCollider.hpp"
+
+#include "../../Util/UtilType.hpp"
+
+#include <list>
+
+
 namespace JirungEngine {
     class Object {
+        public: std::string id;
+        public: Point position;
+        public: BoxCollider* box_collider;
+        public: Object* parent_object;
+        private: std::list<Object*> child_object;
 
+        private: static std::list<std::string> object_id_list;
+
+    public:
+        Object(std::string id, const Object* parent_object = nullptr);
+        Object(std::string id, const Point& position = Position { 0, 0 }, const Object* parent_object = nullptr);
+        ~Object();
+
+        Object* getParent();
+        Object* getChildById(std::string id);
     };
 }
 
