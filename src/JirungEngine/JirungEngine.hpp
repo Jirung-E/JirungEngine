ab659d83728fed2cbc7528f203a18b58c2017486
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-05-11 22:33:03 +0900

    JirungEngine

---
+++ src/JirungEngine/JirungEngine.hpp
@@ -0,0 +1,11 @@
+#ifndef __JirungEngine_hpp__
+#define __JirungEngine_hpp__
+
+#include "BoxCollider/BoxCollider.hpp"
+#include "Canvas/Canvas.hpp"
+#include "EventListener/EventListener.hpp"
+#include "Logger/Logger.hpp"
+#include "Object/Object.hpp"
+#include "Vector/Vector.hpp"
+
+#endif
