730f24775d4434200637a580f10bc109f5812987
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-05-12 18:08:52 +0900

    error_fix

--- src/JirungEngine/JirungEngine.hpp
+++ src/JirungEngine/JirungEngine.hpp
@@ -1,7 +1,8 @@
 #ifndef __JirungEngine_hpp__
 #define __JirungEngine_hpp__
 
-#include "BoxCollider/BoxCollider.hpp"
+#include "Collider/Collider.hpp"
+#include "Collider/BoxCollider.hpp"
 #include "Canvas/Canvas.hpp"
 #include "EventListener/EventListener.hpp"
 #include "Logger/Logger.hpp"
