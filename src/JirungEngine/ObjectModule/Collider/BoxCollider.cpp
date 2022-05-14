191c636db90539f38e7408c6f38e1798f91b0c3f
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-05-14 22:16:28 +0900

    to private

--- src/JirungEngine/ObjectModule/Collider/BoxCollider.cpp
+++ src/JirungEngine/ObjectModule/Collider/BoxCollider.cpp
@@ -4,14 +4,18 @@ using namespace JirungEngine;
 
 
 BoxCollider::BoxCollider(const Point& top_left, const Point& buttom_right) : Collider { top_left, Vector2D { } }, 
-                             top_left { top_left }, buttom_right { buttom_right }, 
-                             side {
-                                 Collider { top_left, Vector2D(buttom_right.x - top_left.x, 0) }, 
-                                 Collider { Point(buttom_right.x, top_left.y), Vector2D(0, buttom_right.y - top_left.y) }, 
-                                 Collider { buttom_right, Vector2D(top_left.x - buttom_right.x, 0) }, 
-                                 Collider { Point(top_left.x, buttom_right.y), Vector2D(0, top_left.y - buttom_right.y) }
-                             } {
+    top_left { top_left }, buttom_right { buttom_right }, 
+    side {
+        Collider { top_left, Vector2D(buttom_right.x - top_left.x, 0) }, 
+        Collider { Point(buttom_right.x, top_left.y), Vector2D(0, buttom_right.y - top_left.y) }, 
+        Collider { buttom_right, Vector2D(top_left.x - buttom_right.x, 0) }, 
+        Collider { Point(top_left.x, buttom_right.y), Vector2D(0, top_left.y - buttom_right.y) }
+    } {
+    
+}
 
+BoxCollider::BoxCollider(const BoxCollider& box_collider) : BoxCollider { box_collider.top_left, box_collider.buttom_right } {
+    
 }
 
 
@@ -29,4 +33,20 @@ Collider BoxCollider::getButtomSide() const {
 
 Collider BoxCollider::getLeftSide() const {
     return side[3];
+}
+
+Point BoxCollider::getTopLeftPoint() const {
+    return top_left;
+}
+
+Point BoxCollider::getTopRightPoint() const {
+    return Point(buttom_right.x, top_left.y);
+}
+
+Point BoxCollider::getButtomRightPoint() const {
+    return buttom_right;
+}
+
+Point BoxCollider::getButtomLeftPoint() const {
+    return Point(top_left.x, buttom_right.y);
 }
--- src/JirungEngine/ObjectModule/Collider/BoxCollider.hpp
+++ src/JirungEngine/ObjectModule/Collider/BoxCollider.hpp
@@ -8,18 +8,24 @@
 
 namespace JirungEngine {
     class BoxCollider : public Collider {
-        public: Point top_left;
-        public: Point buttom_right;
+        private: Point top_left;
+        private: Point buttom_right;
         private: Collider side[4];
 
     public:
         BoxCollider(const Point& top_left, const Point& buttom_right);
+        BoxCollider(const BoxCollider& box_collider);
 
     public:
         Collider getTopSide() const;
         Collider getRightSide() const;
         Collider getButtomSide() const;
         Collider getLeftSide() const;
+
+        Point getTopLeftPoint() const;
+        Point getTopRightPoint() const;
+        Point getButtomRightPoint() const;
+        Point getButtomLeftPoint() const;
     };
 }
 
