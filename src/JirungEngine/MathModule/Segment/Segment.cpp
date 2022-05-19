3b3ee0f7e38a5d542c5192cf9f05bb22035fbb48
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-05-19 18:44:27 +0900

    parameter

--- src/JirungEngine/MathModule/Segment/Segment.cpp
+++ src/JirungEngine/MathModule/Segment/Segment.cpp
@@ -4,3 +4,23 @@ using namespace std;
 using namespace JirungEngine;
 
 
+Segment::Segment(Point point, Vector direction) : point { point }, direction { direction } {
+
+}
+
+Segment::Segment(Vector direction) : Segment { Point(0, 0, 0), direction } {
+
+}
+
+
+float Segment::x(float parameter) const {
+    return point.x + vector.x*parameter;
+}
+
+float Segment::y(float parameter) const {
+    return point.y + vector.y*parameter;
+}
+
+float Segment::z(float parameter) const {
+    return point.z + vector.z*parameter;
+}
--- src/JirungEngine/MathModule/Segment/Segment.hpp
+++ src/JirungEngine/MathModule/Segment/Segment.hpp
@@ -7,7 +7,17 @@
 
 namespace JirungEngine {
     class Segment {
+        public: Point point;
+        public: Vector direction;
 
+    public:
+        Segment(Point point, Vector direction);
+        Segment(Vector direction);
+
+    public:
+        float x(float parameter) const;
+        float y(float parameter) const;
+        float z(float parameter) const;
     };
 }
 
