93fdc29cb47732b1093eb44a827e86166cb8ce28
Jirung-E <leejh0109261@gmail.com>
2022-05-30 09:32:11 +0000

    Line

--- src/JirungEngine/MathModule/Line/Line.hpp
+++ src/JirungEngine/MathModule/Line/Line.hpp
@@ -11,7 +11,26 @@ namespace JirungEngine {
         Point point;
         Vector vector;
         
-    
+    public:
+        Line(Point point, Vector vector);
+        Line(Vector vector);
+
+    public:
+        bool isExistPointOfContactWith(const Line& other) const;
+        Point* getPointOfContactWith(const Line& other) const;
+        Point getFootOfPerpendicularFrom(const Point& point) const;
+        Point getFootOfPerpendicularFrom(const Line& line) const;
+        float getDistanceTo(const Point& point) const;
+        float getDistanceTo(const Line& other) const;
+        bool isParallelTo(const Line& other) const;
+
+        static float getDistanceBetween(const Point& point, const Line& line);
+        static float getDistanceBetween(const Line& line, const Point& point);
+        static float getDistanceBetween(const Line& line1, const Line& line2);
+        static Line getNormalOf(const Line& line1, const Line& line2);
+        static bool isParallel(const Line& line1, const Line& line2);
+        static Point getFootOfPerpendicular(const Line& line, const Point& point);
+        static Point getFootOfPerpendicular(const Line& to, const Line& from);
     };
 }
 
