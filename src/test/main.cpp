5e41fa04ac47b2a59ed4a08e25785771043d4f0e
Jirung-E <leejh0109261@gmail.com>
2022-06-06 14:09:43 +0000

    erase TestGame test class

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -6,65 +6,6 @@ using namespace JirungEngine;
 using namespace Util;
 
 
-class TestGame : public Game {
-private:
-    Object o[10];
-    Object ground { "ground" };
-    
-public:
-    TestGame() : Game { Canvas(120, 45) } {
-        play();
-    }
-    
-public:
-    void play() {
-        for(Object& e : o) {
-            e.image = new TextImage { "resource/o.txtimg" };
-            e.position = Point { getRandomFloat(20, canvas.width - 20), getRandomFloat(10, canvas.height - 10) };
-            e.physics.velocity = Vector { getRandomFloat(-0.2, 0.2), getRandomFloat(-0.2, 0.2) };
-            e.physics.gravity = Vector { 0, 9.8f / 1000 };
-        }
-        
-        ground.image = new TextImage { "resource/ground.txtimg" };
-        ground.addCollider(ground.position, Vector(canvas.width, 0, 0));
-        for(int i=0; i<canvas.width; i+=canvas.width) {
-            ground.position = Point { static_cast<float>(i), 40 };
-        }
-        
-        for(Object* o : Object::object_list) {
-            o->update();
-        }
-        
-        while(true) {
-            update();
-            
-            sleep(1000/60);
-        }
-    }
-    
-private:
-    void update() {
-        canvas.clear();
-        for(Object* o : Object::object_list) {
-            canvas.draw(*o);
-        }
-        println(canvas.getByString());
-
-        for(Object* o : Object::object_list) {
-            o->update();
-        }
-        
-        EventListener::collisionCheck();
-        
-        for(Object* o : Object::object_list) {
-            if(o->isCollide()) {
-                o->image = new TextImage { "resource/x.txtimg" };
-            }
-        }
-    }
-};
-
-
 int main() {
     println("\nTest Start!\n\n");
     
