3878927c426ede44d6fef63148759136fb1f2bd7
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-06-06 23:05:34 +0900

    Math.hpp

--- src/JirungEngine/JirungEngine.hpp
+++ src/JirungEngine/JirungEngine.hpp
@@ -13,10 +13,7 @@
 #include "ObjectModule/Physics/Physics.hpp"
 #include "ObjectModule/Object.hpp"
 #include "Logger/Logger.hpp"
-#include "MathModule/Point/Point.hpp"
-#include "MathModule/Vector/Vector2D.hpp"
-#include "MathModule/Vector/Vector.hpp"
-#include "MathModule/Segment/Segment.hpp"
+#include "MathModule/Math.hpp"
 
 
 namespace JirungEngine {
---
+++ src/JirungEngine/MathModule/Math.hpp
@@ -0,0 +1,13 @@
+#ifndef __Math_hpp__
+#define __Math_hpp__
+
+#include "Point/Point.hpp"
+#include "Vector/Vector2D.hpp"
+#include "Vector/Vector.hpp"
+#include "Line/Line.hpp"
+#include "Plane/Plane.hpp"
+#include "Segment/Segment.hpp"
+#include "Panel/Panel.hpp"
+#include "Polygon/Polygon.hpp"
+
+#endif
