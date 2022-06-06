6c3ba9eb64ee3e6d7703d00db4e49f5c0d9d1a38
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-06-06 22:59:39 +0900

    Polygon

---
+++ src/JirungEngine/MathModule/Polygon/Polygon.cpp
@@ -0,0 +1,52 @@
+#include "Polygon.hpp"
+
+#include <cmath>
+
+#include "../Line/Line.hpp"
+
+using namespace std;
+using namespace JirungEngine;
+
+
+Polygon::Polygon() : p1 { 0, 1, 0 }, p2 { -1.0f*cos(M_PI/6), -1.0f/2, 0 }, p3 { 1.0f*cos(M_PI/6), -1.0f/2, 0 } {
+
+}
+
+
+Vector Polygon::getNormal() const {
+    return Vector::crossProduct(Vector(p2-p1), Vector(p3-p1)).getUnitVector();
+}
+
+Point Polygon::getCenterOfGravity() const {
+    Point center_of_p2_p3 { (p2 + p3) / 2 };
+    Vector p1_to_h { center_of_p2_p3 - p1 };
+    p1_to_h *= 2.0f/3.0f;
+    return p1 + Point { p1_to_h.x, p1_to_h.y, p1_to_h.z };
+}
+
+
+void Polygon::moveTo(const Point& point) {
+    p1 = point;
+    p2 += -p1 + point;
+    p3 += -p1 + point;
+}
+
+void Polygon::transformNormalVectorTo(const Vector& vector) {
+    Vector v1 { normal_vector };
+    Vector v2 { vector };
+    Line n { Vector::crossProduct(v1, v2) };
+    float theta = Vector::getAngleBetween(v1, v2);
+
+    p1 = Line::rotate(p1, n, theta);
+    p2 = Line::rotate(p2, n, theta);
+    p3 = Line::rotate(p3, n, theta);
+
+    normal_vector = vector;
+}
+
+void Polygon::rotate(float radian) {
+    Line n { normal_vector };
+    p1 = Line::rotate(p1, n, radian);
+    p2 = Line::rotate(p2, n, radian);
+    p3 = Line::rotate(p3, n, radian);
+}
---
+++ src/JirungEngine/MathModule/Polygon/Polygon.hpp
@@ -0,0 +1,28 @@
+#ifndef __Polygon_hpp__
+#define __Polygon_hpp__
+
+#include "../Point/Point.hpp"
+#include "../Vector/Vector.hpp"
+
+
+namespace JirungEngine {
+    class Polygon {
+    public:
+        Point p1;
+        Point p2;
+        Point p3;
+
+    public:
+        Polygon();
+
+    public:
+        Vector getNormal() const;
+        Point getCenterOfGravity() const;
+
+        void moveTo(const Point& point);
+        void transformNormalVectorTo(const Vector& vector);
+        void rotate(float radian);
+    };
+}
+
+#endif
