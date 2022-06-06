73e0a15acb5bd216dcbabb80845f05a966a89dd8
Jirung-E <leejh0109261@gmail.com>
2022-06-06 15:13:11 +0000

    22.06.06 폴리곤, 회전

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -11,8 +11,21 @@ int main() {
     
     Canvas3D canvas { 120, 40 };
     
+    Polygon polygon;
+    polygon.moveTo(polygon.p1 + Point(-20, 0, 0));
+    polygon *= 10;
+    
+    Line axis { Point { 0, 0, 0 }, Vector::j() };
+    
     for(int i=0; i<1000; ++i) {
         canvas.clear();
+        
+        canvas.Canvas::draw(to_string(i), 0, 0);
+        canvas.draw(polygon);
+        
+        polygon.rotate(Line(polygon.getCenterOfGravity(), polygon.getNormal()), 0.1);
+        polygon.rotate(Line(polygon.getCenterOfGravity(), Vector::j()), 0.1);
+        polygon.rotate(axis, 0.05);
 
         println(canvas.getByString());
         sleep(1000/30);
