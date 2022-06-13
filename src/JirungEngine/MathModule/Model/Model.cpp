ddbdb4edaa656624852896fe43dfdc3936752135
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-06-13 23:57:55 +0900

    Model : Geometry

--- src/JirungEngine/MathModule/Model/Model.cpp
+++ src/JirungEngine/MathModule/Model/Model.cpp
@@ -4,40 +4,17 @@ using namespace std;
 using namespace JirungEngine;
 
 
-Model::Model() : position { 0, 0, 0 }, 
-pivot { 0, 0, 0 }, x_axis { pivot, Vector::i() }, y_axis { pivot, Vector::j() }, z_axis { pivot, Vector::k() }, 
-polygons { Polygon { } } {
+Model::Model() : polygons { Polygon { } } {
     polygons[0].moveTo({ 0, 1, 0 });
 }
 
 
-Point Model::getPivot() const {
-    return pivot;
-}
-
-void Model::setPivot(const Point& point) {
-    pivot = point;
-    x_axis.point = point;
-    y_axis.point = point;
-    z_axis.point = point;
-}
-
-
 void Model::moveTo(const Point& point) {
-    Point p { point - position };
-
-    position = point;
-
-    pivot += p;
-    x_axis.point += p;
-    y_axis.point += p;
-    z_axis.point += p;
-
+    Point p { point - getPosition() };
     for(Polygon& e : polygons) {
         e.moveTo(e.p1 + p);
     }
-
-    position = point;
+    Geometry::moveTo(point);
 }
 
 
@@ -45,37 +22,22 @@ void Model::moveTo(const Point& point) {
 
 
 void Model::rotateX(float radian) {
-    rotate(x_axis, radian);
+    rotate(getXAxis(), radian);
 }
 
 void Model::rotateY(float radian) {
-    rotate(y_axis, radian);
+    rotate(getYAxis(), radian);
 }
 
 void Model::rotateZ(float radian) {
-    rotate(z_axis, radian);
+    rotate(getZAxis(), radian);
 }
 
 void Model::rotate(const Line& axis, float radian) {
+    Geometry::rotate(axis, radian);
     for(Polygon& e : polygons) {
         e.rotate(axis, radian);
     }
-    x_axis.vector = Vector { Line::rotate(x_axis.point + Point(x_axis.vector.x, x_axis.vector.y, x_axis.vector.z), axis, radian) - x_axis.point };
-    y_axis.vector = Vector { Line::rotate(y_axis.point + Point(y_axis.vector.x, y_axis.vector.y, y_axis.vector.z), axis, radian) - y_axis.point };
-    z_axis.vector = Vector { Line::rotate(z_axis.point + Point(z_axis.vector.x, z_axis.vector.y, z_axis.vector.z), axis, radian) - z_axis.point };
-}
-
-
-Line Model::getXAxis() const {
-    return x_axis;
-}
-
-Line Model::getYAxis() const {
-    return y_axis;
-}
-
-Line Model::getZAxis() const {
-    return z_axis;
 }
 
 
@@ -83,8 +45,8 @@ Model Model::operator*(float n) const {
     Model m { *this };
 
     for(Polygon& e : m.polygons) {
-        Point pivot_to_polygon { e.p1 - pivot };
-        e.moveTo(pivot + pivot_to_polygon * n);
+        Point pivot_to_polygon { e.p1 - getPivot() };
+        e.moveTo(getPivot() + pivot_to_polygon * n);
         e *= n;
     }
     
@@ -95,8 +57,8 @@ Model Model::operator/(float n) const {
     Model m { *this };
 
     for(Polygon& e : m.polygons) {
-        Point pivot_to_polygon { e.p1 - pivot };
-        e.moveTo(pivot + pivot_to_polygon / n);
+        Point pivot_to_polygon { e.p1 - getPivot() };
+        e.moveTo(getPivot() + pivot_to_polygon / n);
         e /= n;
     }
     
@@ -105,8 +67,8 @@ Model Model::operator/(float n) const {
 
 Model Model::operator*=(float n) {
     for(Polygon& e : polygons) {
-        Point pivot_to_polygon { e.p1 - pivot };
-        e.moveTo(pivot + pivot_to_polygon * n);
+        Point pivot_to_polygon { e.p1 - getPivot() };
+        e.moveTo(getPivot() + pivot_to_polygon * n);
         e *= n;
     }
     
@@ -115,8 +77,8 @@ Model Model::operator*=(float n) {
 
 Model Model::operator/=(float n) {
     for(Polygon& e : polygons) {
-        Point pivot_to_polygon { e.p1 - pivot };
-        e.moveTo(pivot + pivot_to_polygon / n);
+        Point pivot_to_polygon { e.p1 - getPivot() };
+        e.moveTo(getPivot() + pivot_to_polygon / n);
         e /= n;
     }
     
--- src/JirungEngine/MathModule/Model/Model.hpp
+++ src/JirungEngine/MathModule/Model/Model.hpp
@@ -2,44 +2,28 @@
 #define __Model_hpp__
 
 #include "../Polygon/Polygon.hpp"
+#include "../Geometry/Geometry.hpp"
 
 #include <vector>                                   // list?
 
 
 namespace JirungEngine {
-    class Model {
-    private:
-        Point position;                             // 포지션이 바뀔경우 축들의 위치도 바뀌어야 하므로 프라이빗으로 설정. 나중에 바뀔수도..
-
-        Point pivot;                                // 필요 없을수도?
-        Line x_axis;
-        Line y_axis;
-        Line z_axis;
-
-        // unsigned int num_of_polygons;
+    class Model : public Geometry {
     public:
         std::vector<Polygon> polygons;
-        
 
     public:
         Model();
 
     public:
-        Point getPivot() const;
-        void setPivot(const Point& point);          // 중심축을 바꿈
-
         void moveTo(const Point& point);            // 중심축의 위치를 기준으로 모델을 이동시킴. Polygon들의 위치를 이동시켜야해서 반드시 필요. 때문에 position은 반드시 private
-
-        // void addPolygon(const Polygon& polygon);     // 이게 필요할까?
 
         void rotateX(float radian);
         void rotateY(float radian);
         void rotateZ(float radian);
         void rotate(const Line& axis, float radian);
-
-        Line getXAxis() const;
-        Line getYAxis() const;
-        Line getZAxis() const;
+        
+        // void addPolygon(const Polygon& polygon);     // 이게 필요할까?
 
         Model operator*(float n) const;
         Model operator/(float n) const;
