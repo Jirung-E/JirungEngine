f0637882880dfe2c15993ded2392431e7671642d
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-06-14 22:14:54 +0900

    테스트 작성

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -12,11 +12,6 @@ int main() {
     Viewer* viewer = new ConsoleViewer();
     Renderer renderer;
     Polygon p;
-    Canvas canvas;
-    canvas.default_background = '#';
-    canvas.clear();
-    viewer->show(canvas);
-    canvas = renderer.renderGeneral(p);
-    viewer->show(canvas);
+    renderer.renderGeneral(p);
     viewer->show(renderer.image);
 }
