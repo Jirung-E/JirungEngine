020d409efa7e663e269d6d3c94343c0257e7edd3
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-06-19 23:08:30 +0900

    .

--- src/JirungEngine/JirungEngine.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __JirungEngine_hpp__
-#define __JirungEngine_hpp__
-
-#include "MathModule/Math.hpp"
-#include "GraphicModule/Renderer/Renderer.hpp"
-#include "GraphicModule/Viewer/Viewer.hpp"
-#include "GraphicModule/Viewer/ConsoleViewer.hpp"
-
-#endif
