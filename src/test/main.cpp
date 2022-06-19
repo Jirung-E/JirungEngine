f57cb199deb9f91f25dc65751bf1b2d3fd4ef6ce
Jirung-E <leejh0109261@gmail.com>
2022-06-19 05:59:18 +0000

    ^^

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -9,28 +9,16 @@ using namespace Util;
 int main() {
     println("\nTest Start!\n\n");
 
-    Viewer* viewer = new ConsoleViewer();
-    Renderer renderer;
-    Polygon pol[50];
-    int cnt = 0;
-    for(Polygon& e : pol) {
-        e.moveTo(e.p1 - Point { 0, 0, cnt * 3.0f });
-        e.rotate(Line(e.getCenterOfGravity(), e.getNormal()), cnt/10.0f);
-        cnt++;
-    }
+    Point p1 { 1, 5, 2 };
+    Point p2 { -1, 9, -7 };
+    Vector v { 1, 1, 1 };
     
-    for(int i=0; i<1000; ++i) {
-        for(const Polygon& e : pol) {
-            renderer.renderClear(e);
-        }
-        println(to_string(i));
-        viewer->show(*renderer.image);
-        for(Polygon& e : pol) {
-            e.moveTo(e.p1 + Point(0, 0, 0.5));
-            e.rotate(Line(e.getCenterOfGravity(), e.getNormal()), 0.1);
-        }
-        
-        renderer.clearImage();
-        sleep(1000/30);
-    }
+    printf("%.2f %.2f %.2f\n", p2.x, p2.y, p2.z);
+    Point p3 = p1 + v;
+    printf("%f %f %f\n", p3.x, p3.y, p3.z);
+    p3 += v;
+    printf("%f %f %f\n", p3.x, p3.y, p3.z);
+    
+    Point po { v };
+    printf("%f %f %f\n", po.x, po.y, po.z);
 }
