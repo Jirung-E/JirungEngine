2d24d83abe63e697db7cb8c9a7171d19a0c0e561
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-06-20 15:42:15 +0900

    Util

--- src/Util/Util.hpp
+++ src/Util/Util.hpp
@@ -1,8 +1,19 @@
 #ifndef __Util_hpp__
 #define __Util_hpp__
 
+#include <iostream>
+
+#include "UtilType.hpp"
+
+
 namespace Util {
+    void print(str_t text = " ");
+    void println(str_t text = "");
+
+    void sleep(unsigned short time);
     
+    int getRandomInt(int from, int to);
+    float getRandomFloat(float from, float to);
 }
 
 #endif
