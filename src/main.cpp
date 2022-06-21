4c94478072468ea546cc9295e1ca26d5a5a5d9e9
Jirung-E <leejh0109261@gmail.com>
2022-06-21 09:41:41 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,23 @@ using namespace std;
 using namespace Util;
 
 
-int main() {
+void testUtil() {
     cout << "Test Start" << endl;
     
     for(int i=0; i<100; ++i) {
-        float f = getRandomFloat(-10.0f, 14.4f);
-        println(to_string(f));
+        println(to_string(getRandomFloat(-10.0f, 14.4f)));
     }
     for(int i=0; i<100; ++i) {
-        int in = getRandomInt(-5, 20);
-        println(to_string(in));
+        println(to_string(getRandomInt(-5, 20)));
     }
     println();
     
+    println("sleep for 5000 milliseconds");
     sleep(5000);
     println("Test End");
+}
+
+
+int main() {
+    testUtil();
 }
