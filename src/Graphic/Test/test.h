36df1a34ed8936514b56d10bdfd41765873ae0a8
Jirung-E <leejh0109261@gmail.com>
2022-07-24 02:22:51 +0000

    new test

--- src/Graphic/Test/test.h
+++ src/Graphic/Test/test.h
@@ -1,6 +1,6 @@
 #include "../Graphic.hpp"
 
-#include <thread>
+//#include <thread>
 
 using namespace std;
 using namespace Graphic;
@@ -8,5 +8,15 @@ using namespace Graphic;
 
 void test() {
     Display display;
+    display.setPixelBrightness(1, 5, 5);
+    display.setPixelBrightness(2, 6, 5);
+    display.setPixelBrightness(3, 7, 5);
+    display.setPixelBrightness(4, 8, 5);
+    display.setPixelBrightness(5, 9, 5);
+    puts(display.getByString().c_str());
     
+    puts("\n-------------------------------------------------------------------------\n");
+    
+    display.clear();
+    puts(display.getByString().c_str());
 }
