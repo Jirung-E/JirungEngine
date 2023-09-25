6cc1031603c2bdf021f105d1b199dea69ab3caea
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-09-25 16:56:22 +0900

    Bug fix - including deleted file "Panel.hpp"

--- src/Math/Math.hpp
+++ src/Math/Math.hpp
@@ -6,7 +6,6 @@
 #include "Line/Line.hpp"
 #include "Plane/Plane.hpp"
 #include "Segment/Segment.hpp"
-#include "Panel/Panel.hpp"
 #include "Polygon/Polygon.hpp"
 #include "Model/Model.hpp"
 #include "Geometry/Geometry.hpp"
